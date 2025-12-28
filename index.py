fecc3e1b2bfac0c502571ee6c3e4f04d10509ebf
nahannazer-git <nahannazerthadathil@gmail.com>
2025-12-28 22:26:45 +0530

    Configure Vercel with api/index.py entrypoint

--- index.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Create the Flask application instance
-# Vercel looks for the 'app' or 'application' variable in this file
-app = create_app()
-
-if __name__ == "__main__":
-    app.run()
